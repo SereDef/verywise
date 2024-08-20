93992c31cac2d63c3a4fd19cb3f26e90208b9704
SereDef <sere.def@gmail.com>
2024-08-20 15:35:25 +0200

    Add startup message

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+# Start-up message
+
+.onAttach <- function(libname, pkgname) {
+  version <- read.dcf(file=system.file("DESCRIPTION", package=pkgname),
+                      fields="Version")
+  packageStartupMessage("Welcome, ", pkgname," user!\nThis is version: ",version,
+  "\nFor questions, issues, and bug reports, please see https://github.com/SereDef/verywise")
+}
